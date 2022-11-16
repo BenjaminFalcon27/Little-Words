d6717e583d81cb59a0c945d55e5addc64e1ccaac
BenjaminFalcon27 <85941055+BenjaminFalcon27@users.noreply.github.com>
2022-11-16 12:26:18 +0100

    merge correction

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column{
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
             Text(
