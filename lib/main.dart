6a72ac3ae2ecf25332fc9e8322232d1c7ee9f1d7
BenjaminFalcon27 <85941055+BenjaminFalcon27@users.noreply.github.com>
2022-11-23 11:39:00 +0100

    delete unused

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_map/plugin_api.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:little_words/home/home.view.dart';
 import 'package:location/location.dart';
-import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
