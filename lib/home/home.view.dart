17d192c29d94ad97227e36dfc9c50d9031547d8d
BenjaminFalcon27 <85941055+BenjaminFalcon27@users.noreply.github.com>
2022-11-30 11:58:07 +0100

    model / DTO created

--- lib/home/home.view.dart
+++ lib/home/home.view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:little_words/dio.provider.dart';
-import 'package:little_words/version.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomeView extends ConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -9,27 +10,19 @@ class HomeView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Version(),
-          ElevatedButton(
-              onPressed: () async {
-                final AsyncValue<String> value = ref.read(versionProvider);
-                value.when(data: (data) {
-                  print('response: $data');
-                }, error: (object, stackTrace) {
-                  print(object.toString());
-                  print(stackTrace.toString());
-                }, loading: () {
-                  print("loading");
-                });
-              },
-              child: const Text('Get backend version')),
-        ],
-      )),
+      body: Container(
+        color: Colors.red,
+        child: Expanded(
+            child: FlutterMap(
+          options: MapOptions(center: LatLng(50.95129, 1.858686), zoom: 18.0),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
---
+++ lib/models/word.model.dart
@@ -0,0 +1,31 @@
+class Word {
+  final int? uid;
+  final String? author;
+  final String? content;
+  final String? latitude;
+  final String? longitude;
+
+  const Word({
+    required this.uid,
+    required this.author,
+    required this.content,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  factory Word.fromJson(Map<String, dynamic> json) => Word(
+        uid: json['uid'],
+        author: json['author'],
+        content: json['content'],
+        latitude: json['latitude'],
+        longitude: json['longitude'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'uid': uid,
+        'author': author,
+        'content': content,
+        'longitude': longitude,
+        'latitude': latitude
+      };
+}
