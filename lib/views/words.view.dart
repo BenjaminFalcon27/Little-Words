24c12465d78931849369d08ac63b9ee488e39127
DaruniaIsBack <75274590+DaruniaIsBack@users.noreply.github.com>
2022-12-14 11:56:02 +0100

    ff
    ff

--- lib/views/words.view.dart
+++ lib/views/words.view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swipe_action_cell/core/cell.dart';
 
 import '../models/word.model.dart';
 import '../service/database.helper.dart';
 import '../widgets/word.widget.dart';
 import 'word.view.dart';
+import 'package:swipe/swipe.dart';
 
 class WordsScreen extends StatefulWidget {
   const WordsScreen({Key? key}) : super(key: key);
@@ -40,49 +42,50 @@ class _WordsScreenState extends State<WordsScreen> {
               if (snapshot.data != null) {
                 return ListView.builder(
                   itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) => WordWidget(
-                    word: snapshot.data![index],
-                      onTap: () async {
-                      await Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => WordScreen(
-                                    word: snapshot.data![index],
-                                  )));
-                      setState(() {});
-                    },
-
-                    onLongPress: () async {
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: const Text(
-                                  'Are you sure you want to delete this word?'),
-                              actions: [
-                                ElevatedButton(
-                                  style: ButtonStyle(
-                                      backgroundColor:
+                  itemBuilder: (context, index) =>
+                      WordWidget(
+                        word: snapshot.data![index],
+                        onTap: () async {
+                          await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      WordScreen(
+                                        word: snapshot.data![index],
+                                      )));
+                          setState(() {});
+                        },
+                        onLongPress: () async {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text(
+                                      'Are you sure you want to delete this word?'),
+                                  actions: [
+                                    ElevatedButton(
+                                      style: ButtonStyle(
+                                          backgroundColor:
                                           MaterialStateProperty.all(
                                               Colors.red)),
-                                  onPressed: () async {
-                                    await DatabaseHelper.deleteWord(
-                                        snapshot.data![index]);
-                                    // ignore: use_build_context_synchronously
-                                    Navigator.pop(context);
-                                    setState(() {});
-                                  },
-                                  child: const Text('Yes'),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () => Navigator.pop(context),
-                                  child: const Text('No'),
-                                ),
-                              ],
-                            );
-                          });
-                    },
-                  ),
+                                      onPressed: () async {
+                                        await DatabaseHelper.deleteWord(
+                                            snapshot.data![index]);
+                                        // ignore: use_build_context_synchronously
+                                        Navigator.pop(context);
+                                        setState(() {});
+                                      },
+                                      child: const Text('Yes'),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text('No'),
+                                    ),
+                                  ],
+                                );
+                              });
+                        },
+                      ),
                 );
               }
               return const Center(
