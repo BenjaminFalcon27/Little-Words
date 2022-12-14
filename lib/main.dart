bf8d1bae8c36d99554affb9b7f1200922d46d8c0
PaulDlcr <delacrepaul@gmail.com>
2022-12-14 12:01:27 +0100

    Adding User Localisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:little_words/views/home.view.dart';
 import 'package:little_words/themes/colors.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -17,6 +19,28 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: MyTheme.defaultTheme,
       home: const HomeView(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, this.title});
+
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const <Widget>[
+            Divider(height: 32),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/home.view.dart
+++ lib/views/home.view.dart
@@ -3,28 +3,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:little_words/widgets/map.dart';
+import 'package:location/location.dart';
 
 import 'words.view.dart';
 
 class HomeView extends ConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Container(
-        color: Colors.red,
-        child: Expanded(
-            child: FlutterMap(
-          options: MapOptions(center: LatLng(50.95129, 1.858686), zoom: 18.0),
-          children: [
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-            ),
-          ],
-        )),
-      ),
+      body: LittlewordsMap(),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await Navigator.push(context,
@@ -36,4 +27,23 @@ class HomeView extends ConsumerWidget {
       ),
     );
   }
+}
+
+Location location = new Location();
+
+late bool _serviceEnabled;
+late PermissionStatus _permissionGranted;
+late LocationData _locationData;
+
+Future<dynamic> getLocation() async{
+  _serviceEnabled = await location.serviceEnabled();
+  if (!_serviceEnabled) _serviceEnabled = await location.requestService();
+
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+    _permissionGranted = await location.requestPermission();
+  }
+
+  _locationData = await location.getLocation();
+  return _locationData;
 }
