df8bbc2c272d63b99a00974aa60b86978f22a9bd
PaulDlcr <delacrepaul@gmail.com>
2023-01-03 16:27:43 +0100

    Added + Button

--- lib/views/home.view.dart
+++ lib/views/home.view.dart
@@ -1,29 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map/plugin_api.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:little_words/views/word.view.dart';
 import 'package:little_words/widgets/map.dart';
 import 'package:location/location.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 import 'words.view.dart';
 
 class HomeView extends ConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: LittlewordsMap(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const WordsScreen()));
-          //TODO Ajouter code pour ouvrir sac
-        },
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: const IconThemeData(size: 22),
         backgroundColor: Theme.of(context).primaryColor,
-        child: const Icon(Icons.backpack),
+        visible: true,
+        curve: Curves.bounceIn,
+        children: [
+          SpeedDialChild(
+              child: const Icon(Icons.backpack),
+              backgroundColor: Theme.of(context).primaryColor,
+              onTap: () async {
+                await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const WordsScreen()));
+              },
+              label: 'Sac à mots',
+              labelStyle: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  fontSize: 16.0),
+              labelBackgroundColor: const Color(0xFF801E48)),
+          // FAB 2
+          SpeedDialChild(
+              child: Icon(Icons.add),
+              backgroundColor: Theme.of(context).primaryColor,
+              onTap: () async {
+                await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const WordScreen()));
+              },
+              label: 'Nouveau mot',
+              labelStyle: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  fontSize: 16.0),
+              labelBackgroundColor: const Color(0xFF801E48))
+        ],
       ),
     );
   }
@@ -35,7 +64,7 @@ late bool _serviceEnabled;
 late PermissionStatus _permissionGranted;
 late LocationData _locationData;
 
-Future<dynamic> getLocation() async{
+Future<dynamic> getLocation() async {
   _serviceEnabled = await location.serviceEnabled();
   if (!_serviceEnabled) _serviceEnabled = await location.requestService();
 
